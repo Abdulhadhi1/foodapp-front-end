0c89f181c5d5b7e62714f90fa60b5ac796d25137
Abdul Hadhi <108865426+Abdulhadhi1@users.noreply.github.com>
2023-01-18 13:14:34 +0530

    Update LoginScreen.js

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -55,9 +55,9 @@ export default function LoginScreen() {
             >
               LOGIN
             </button><br />
-           <p> User:test123@gmail.com</p><br/>
-           <p> Password:12345</p>
-            <br />    <br />
+//            <p> User:test123@gmail.com</p><br/>
+//            <p> Password:12345</p>
+            <br /><br />
             <a style={{ color: "black" }} href="/register" className="lg-title">
               Click Here To Register
             </a>
